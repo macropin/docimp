66794ec360d033d85c5ae2891fc157e27abfbdaf
Andrew Cutler <macropin@gmail.com>
2012-04-13 17:23:27 +1000

    Use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 
 setup(name='docimp',
-      version='1.0',
+      version='0.1.0',
       url='https://github.com/macropin/docimp',
       packages=['docimp',],
       install_requires=['couchdbkit',],
