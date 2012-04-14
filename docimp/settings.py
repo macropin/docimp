afa81d245dd780e2224fce70c890a23b2e0e66f0
Andrew Cutler <macropin@gmail.com>
2012-04-14 13:51:02 +1000

    Update dependency links for renamed, lorem_pdf

--- docimp/settings.py
+++ docimp/settings.py
@@ -3,7 +3,7 @@
 COUCH_SERVER_URL='http://192.168.1.124:5984' # Set Me
 COUCH_DATABASE_NAME='pdfs'
 
-DOCMK_URL='http://localhost:8000/pdf/' # Base Url to our 'docmk' pdf generator 
+DOCMK_URL='http://localhost:8000/pdf/' # Base Url to our lorem pdf generator 
 
 
 try:
--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@
 from setuptools import setup
 
 setup(name='docimp',
-      version='0.1.0',
-      url='https://github.com/macropin/docimp',
-      packages=['docimp',],
-      install_requires=['couchdbkit',],
-     )
+    version='0.1.1',
+    url='https://github.com/macropin/docimp',
+    packages=['docimp',],
+    install_requires=['couchdbkit', 'lorem_pdf'],
+    dependency_links = [
+            "https://github.com/macropin/django-lorem-pdf/tarball/master#egg=lorem_pdf"
+        ],
+    )
